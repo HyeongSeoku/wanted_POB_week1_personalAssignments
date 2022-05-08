5375e25832e82fa75c9e62103ce8df5e13302296
HyeongSeoku <gudtjr3437@naver.com>
2022-05-08 12:27:52 +0900

    Feat : Input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,98 @@
+import { useEffect, useRef, useState } from 'react'
+import styles from './Input.module.scss'
+import { faCheckCircle, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Input() {
+  const [email, setEmail] = useState('')
+  const [emailValid, setEmailValid] = useState(false)
+  const [emailFocus, setEmailFocus] = useState(false)
+  const [pwd, setPwd] = useState('')
+  const [pwdVisible, setPwdVisible] = useState(false)
+  const emailRef = useRef()
+  const pwdRef = useRef()
+
+  const onFocusEmail = (e) => {
+    setEmailFocus(true)
+  }
+
+  const onBlurEmail = (e) => {
+    setEmailFocus(false)
+  }
+
+  const isEmail = (asValue) => {
+    const regExp = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,8})+$/
+
+    return regExp.test(asValue)
+  }
+
+  const onChangeEmail = (e) => {
+    const { value } = e.currentTarget
+    setEmail(value)
+    isEmail(value) ? setEmailValid(true) : setEmailValid(false)
+  }
+
+  const onChangePwd = (e) => {
+    const { value } = e.currentTarget
+    setPwd(value)
+  }
+
+  const onClickEyeIcon = () => {
+    pwdRef.current.focus()
+    setPwdVisible((current) => !current)
+  }
+
+  const handleBlurPwd = () => {
+    setPwdVisible(false)
+  }
+
+  return (
+    <section className={styles.inputSection}>
+      <h2 className={styles.title}>인풋</h2>
+      <div className={styles.tagContainer}>
+        <span className={styles.tag}>E-mail</span>
+      </div>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.emailInput}
+          placeholder='Email'
+          type='text'
+          value={email}
+          onChange={onChangeEmail}
+          ref={emailRef}
+          onFocus={onFocusEmail}
+          onBlur={onBlurEmail}
+        />
+        <span className={emailValid ? styles.correctIcon : styles.icon}>
+          <FontAwesomeIcon icon={faCheckCircle} />
+        </span>
+      </div>
+
+      <div className={styles.validMsg}>{!emailValid && !emailFocus ? 'Invalid email address' : ' '}</div>
+      <div className={styles.tagContainer}>
+        <span className={styles.tag}>Password</span>
+      </div>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.pwdInput}
+          placeholder='Password'
+          type={pwdVisible ? 'text' : 'password'}
+          value={pwd}
+          onChange={onChangePwd}
+          ref={pwdRef}
+          onBlur={handleBlurPwd}
+        />
+        <span
+          role='button'
+          tabIndex='0'
+          className={pwdVisible ? styles.correctIcon : styles.icon}
+          onClick={onClickEyeIcon}
+        >
+          {pwdVisible ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
+        </span>
+      </div>
+    </section>
+  )
+}
+
+export default Input
