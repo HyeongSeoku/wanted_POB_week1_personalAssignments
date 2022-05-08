8528ea2b4d6d1804d09f46fef8e5f4a4f0809c73
HyeongSeoku <gudtjr3437@naver.com>
2022-05-08 16:02:54 +0900

    Chore : util files

---
+++ src/util/constants/componentsData.js
@@ -0,0 +1,42 @@
+import potatoImg from '../../imgs/potato.png'
+import sweetPotatoImg from '../../imgs/sweetPotato.png'
+import curyImg from '../../imgs/cury.png'
+
+export const toggleData = ['기본', '상세']
+
+export const tabData = [
+  { id: 'tab_1', title: '감자', contents: potatoImg },
+  { id: 'tab_2', title: '고구마', contents: sweetPotatoImg },
+  { id: 'tab_3', title: '카레라이스', contents: curyImg },
+]
+
+export const sliderData = [
+  { id: 'slider_1', data: 1, tabIndex: 0 },
+  { id: 'slider_2', data: 25, tabIndex: 1 },
+  { id: 'slider_3', data: 50, tabIndex: 2 },
+  { id: 'slider_4', data: 75, tabIndex: 3 },
+  { id: 'slider_5', data: 100, tabIndex: 4 },
+]
+
+export const dropDownData = [
+  'All Teams',
+  'Manchester United',
+  'Manchester City',
+  'Liverpool',
+  'Chelsea',
+  'Tottenham HotSpur',
+  'Everton',
+  'NewCastle United',
+  'Leicester City',
+  'Aresnal',
+  'WestHam United',
+  'Leeds United',
+  'Southampton',
+  'Crystal Palace',
+  'Burnley',
+  'Brighton & Hove Albion',
+  'Norwich',
+  'Watford',
+  'BrentFord',
+  'Wolverhampton',
+]
